3aded30549ab94706d06b6ab32bbea36b768c4bc
subsubsubx <dptwink@gmail.com>
2023-12-15 21:06:23 +0300

    Add task 1

---
+++ sandbox/src/main/java/ru/stqa/geometry/Triangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.geometry;
+
+public record Triangle(double a, double b, double c) {
+
+    public double heronsFormula() {
+        double p = (this.a + this.b + this.c) / 2;
+        return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    }
+
+    public double perimeter() {
+        return this.a + this.b + this.c;
+    }
+}
+
