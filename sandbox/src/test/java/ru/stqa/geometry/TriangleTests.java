aeca54867e6ae472862be721ead5cce50ed490d9
subsubsubx <dptwink@gmail.com>
2023-12-22 20:01:31 +0300

    task 2.1

--- sandbox/src/test/java/ru/stqa/geometry/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/TriangleTests.java
@@ -14,4 +14,13 @@ public class TriangleTests {
     void testPerimeter() {
         Assertions.assertEquals(12, new Triangle(3, 4, 5).perimeter());
     }
+
+    @Test
+    void triangleEquals() {
+     Triangle tri1 = new Triangle(1, 2, 3);
+     Triangle tri2 = new Triangle(3, 2, 1);
+     Assertions.assertTrue(tri1.equals(tri2));
+    }
+
 }
+
